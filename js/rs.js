320cb6e707edf5dd8939737fc1caf2667c483aae
Roger Stringer <freekrai@me.com>
2016-02-17 21:53:04 -0800

    Update rs.js

--- js/rs.js
+++ js/rs.js
@@ -192,10 +192,28 @@ mostPopular.prototype.updatePage = function(url, title){
 	//	get current count and increment it...
 	var cnt = 0;
 	_this.flybaseRef.where({"key": key}).orderBy( {"views":-1} ).on('value').then( function( data ){
+/*
 		var item = data.first().value();
 		item.views = item.views + 1;
 		_this.flybaseRef.update(item._id,item, function(resp) {
 			console.log( key + " updated" );
+		});
+*/
+		var first = true;
+		data.forEach( function(snapshot) {
+			var item = snapshot.value();
+			if( first ){
+				item.views = item.views + 1;
+				_this.flybaseRef.update(item._id,item, function(resp) {
+					console.log( key + " updated" );
+				});
+			}else{
+				// clean up any dupes until we make this better...
+				_this.flybaseRef.deleteDocument(item._id, function(resp) {
+					console.log( item._id + " deleted");
+				});
+			}
+			first = false;
 		});
 	},function(){
 		// no count, so never added before..
